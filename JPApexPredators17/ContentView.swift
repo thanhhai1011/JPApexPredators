7f52526bcdc54f96a3b4e2c8f537c386c8a95664
hai.nguyen <hai.nguyen@dgvdigital.com>
2024-10-04 11:35:03 +0700

    Initial Commit

---
+++ JPApexPredators17/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JPApexPredators17
+//
+//  Created by Hai Nguyen on 4/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JPApexPredators17/JPApexPredators17App.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexPredators17App.swift
+//  JPApexPredators17
+//
+//  Created by Hai Nguyen on 4/10/24.
+//
+
+import SwiftUI
+
+@main
+struct JPApexPredators17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
